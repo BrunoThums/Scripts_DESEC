78e235758ff483d82905fd1fc6300c21dd6028d4
Bruno Brill Thums <bruno.thums@universo.univates.br>
2024-01-23 18:18:03 -0300

    Update host_all.sh

--- host_all.sh
+++ host_all.sh
@@ -6,14 +6,42 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+print(){
+    echo -e "\e[35m" $1 "\e[0m"
+}
+
 automatize_host(){
-    host -t A $site
-    host -t mx $site
-    host -t ns $site
+    print "IPv4"
+    host -t A $site | cut -d " " -f4 | sed 's/.$//'
+    print ""
+    
+    print "IPv6"
+    host -t AAAA $site | egrep -v "has no" | cut -d " " -f5 | sed 's/.$//'
+    print ""
+        
+    print "Servidor (es) de e-mail"
+    host -t mx $site | cut -d " " -f7 | sed 's/.$//'
+    print ""
+        
+    print "Servidores"
+    host -t ns $site | cut -d " " -f4 | sed 's/.$//' # Nomes de Servidores. ns1 -> registro de entradas de DNS (CNAME, registros de IP, subdomínios…) | ns2 -> backup
+    print ""
+
+    print "Informações do host"
     host -t hinfo $site
-    host -t AAAA $site
+    print ""
+    
+    print "Configuração de SPF"
     host -t txt $site
-    host -t l $site
+    print ""    
+
+    print "AXFR"
+    host -l $site | egrep -v "failed"
+    print ""
+        
+    # aqui precisa colocar subdomínios, senão NUNCA vai funcionar. Os ALIAS são específicos para subdomínios, isso INCLUI o www        
+    print "CNAME/ALIAS (use com um subdomínio: host -t cname {site}"
+    host -t cname $site | egrep -v "has no" 
 }
 
 if [ "$1" ]; then
