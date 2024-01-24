955e56d9e043e37ed90d8e4d2e78d38c44d2637d
Bruno Brill Thums <bruno.thums@universo.univates.br>
2024-01-24 09:09:45 -0300

    Added SPF verification and print before results

--- host_all.sh
+++ host_all.sh
@@ -10,6 +10,52 @@ print(){
     echo -e "\e[35m" $1 "\e[0m"
 }
 
+verificar_spf() {
+    local minha_string="$*"
+    local posicao_all=$(expr index "$minha_string" "all")
+
+    if [ "$posicao_all" -gt 1 ]; then
+        caractere_anterior=$(echo "$minha_string" | cut -c "$((posicao_all - 1))")
+        
+        case "$caractere_anterior" in
+            "-")
+                echo -e "\e[92m RESTRITIVO: - \e[0m"
+                ;;
+            "~")
+                echo -e "\e[93m SEMI-RESTRITIVO: ~ \e[0m"
+                ;;
+            "?")
+                echo -e "\e[91m SUSCETÍVEL: ? \e[0m"
+                ;;
+            *)
+                echo -e "\e[95m LIVRE PARA EXPLORAR! Aparentemente não há configuração! \e[0m"
+                ;;
+        esac
+    else
+        echo -e "\e[95m LIVRE PARA EXPLORAR! Aparentemente não há configuração!! \e[0m"
+    fi
+}
+
+verificar_spf() {
+    spf="$*"
+    # Usando expressão regular para encontrar "all" e capturar o caractere anterior
+    if [[ "$spf" =~ (.)(A|a)(L|l)(L|l) ]]; then
+        caractere_anterior="${BASH_REMATCH[1]}"  # Obtém o último caractere da captura
+        # Verificar o caractere e imprimir a resposta correspondente
+        if [ "$caractere_anterior" = "-" ]; then
+            echo -e "\e[92m SPF RESTRITIVO: - \e[0m"
+        elif [ "$caractere_anterior" = "~" ]; then
+            echo -e "\e[93m SPF SEMI-RESTRITIVO: ~ \e[0m"
+        elif [ "$caractere_anterior" = "?" ]; then
+            echo -e "\e[91m SPF SUSCETÍVEL A MAIL SPOOFING: ? \e[0m"
+        else
+            echo -e "\e[95m SUSCETÍVEL A MAIL SPOOFING! Caractere inválido:" $caractere_anterior "\e[0m"
+        fi
+    else
+        echo -e "\e[95m SUSCETÍVEL A MAIL SPOOFING! Aparentemente não está configurado! \e[0m"
+    fi
+}
+
 automatize_host(){
     print "IPv4"
     host -t A $site | cut -d " " -f4 | sed 's/.$//'
@@ -32,7 +78,9 @@ automatize_host(){
     print ""
     
     print "Configuração de SPF"
-    host -t txt $site
+    spf=$(host -t txt $site)
+    echo "$spf"
+    verificar_spf $spf
     print ""    
 
     print "AXFR"
